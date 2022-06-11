d67a8e17f4dca42ab83fb3aa28602bcdb5d3be28
Qwerty <thehulk489@gmail.com>
2022-06-11 16:25:41 +0530

    added users page

---
+++ lib/pages/users_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class UsersPage extends StatefulWidget {
+  @override
+  State<UsersPage> createState() => _UsersPageState();
+}
+
+class _UsersPageState extends State<UsersPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
