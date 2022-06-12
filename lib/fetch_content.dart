6297fec51b4943c05bb34551b3d97d2bdf66ec25
Qwerty <thehulk489@gmail.com>
2022-06-12 14:33:25 +0530

    add fetch albums

--- lib/fetch_content.dart
+++ lib/fetch_content.dart
@@ -2,6 +2,7 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 import '../models/user_model.dart';
 import '../models/post_model.dart';
+import '../models/album_model.dart';
 
 List<User>? users;
 // Function to fetch  users from the url
@@ -15,7 +16,7 @@ Future<List<User>?> fetchUser() async {
 
     return parsed.map<User>((json) => User.fromJson(json)).toList();
   } else {
-    throw Exception('Failed to load');
+    throw Exception('Failed to load users');
   }
 }
 
@@ -29,5 +30,18 @@ Future<List<Post>?> fetchPostsFromURL() async {
     return parsed.map<Post>((json) => Post.fromJson(json)).toList();
   } else {
     throw Exception('Failed to load posts');
+  }
+}
+
+Future<List<Album>?> fetchAlbumsFromURL() async {
+  final response =
+      await http.get(Uri.parse('http://jsonplaceholder.typicode.com/albums'));
+
+  if (response.statusCode == 200) {
+    final parsed = json.decode(response.body).cast<Map<String, dynamic>>();
+
+    return parsed.map<Album>((json) => Album.fromJson(json)).toList();
+  } else {
+    throw Exception('Failed to load albums');
   }
 }
