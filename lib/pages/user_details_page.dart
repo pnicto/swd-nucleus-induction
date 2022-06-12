dd1070c0104fba015ccc4c8210913b2891243aaa
Qwerty <thehulk489@gmail.com>
2022-06-12 14:30:48 +0530

    removed fetch funcs

--- lib/pages/user_details_page.dart
+++ lib/pages/user_details_page.dart
@@ -1,52 +1,87 @@
 import 'package:flutter/material.dart';
 
-class UserDetailsPage extends StatelessWidget {
+import '../fetch_content.dart';
+import '../models/post_model.dart';
+
+class UserDetailsPage extends StatefulWidget {
   static const routeName = '/user-details';
+
+  const UserDetailsPage({Key? key}) : super(key: key);
+
+  @override
+  State<UserDetailsPage> createState() => _UserDetailsPageState();
+}
+
+class _UserDetailsPageState extends State<UserDetailsPage> {
+  // Control loading spinner
+  var isLoaded = false;
+
+  List<Post>? posts;
+  // Function to fetch all posts
+
+  fetchPosts() async {
+    posts = await fetchPostsFromURL();
+    if (posts != null) {
+      setState(() {
+        isLoaded = true;
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetchPosts();
+  }
 
   Widget buildText(String content) {
     return Text(
       content,
-      style: TextStyle(fontSize: 18),
+      style: const TextStyle(fontSize: 18),
       textAlign: TextAlign.center,
     );
   }
 
-  const UserDetailsPage({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     final user =
         ModalRoute.of(context)!.settings.arguments as Map<String, Object>;
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("${user['name']}'s Details"),
-      ),
-      body: Container(
-        margin: const EdgeInsets.symmetric(vertical: 4),
-        child: Column(
-          children: [
-            Container(
-              alignment: Alignment.center,
-              child: Card(
-                elevation: 5,
-                child: Container(
-                  padding: EdgeInsets.all(15),
-                  child: Column(
-                    children: [
-                      buildText(user['name'] as String),
-                      buildText(user['username'] as String),
-                      buildText(user['email'] as String),
-                      buildText(user['phone'] as String),
-                      buildText(user['website'] as String),
-                      buildText(user['company'] as String),
-                      buildText(user['address'] as String),
-                    ],
+
+    return isLoaded
+        ? Scaffold(
+            appBar: AppBar(
+              title: Text("${user['name']}'s Details"),
+            ),
+            body: Container(
+              margin: const EdgeInsets.symmetric(vertical: 4),
+              child: Column(
+                children: [
+                  Container(
+                    alignment: Alignment.center,
+                    child: Card(
+                      elevation: 5,
+                      child: Container(
+                        padding: const EdgeInsets.all(15),
+                        child: Column(
+                          children: [
+                            buildText(user['name'] as String),
+                            buildText(user['username'] as String),
+                            buildText(user['email'] as String),
+                            buildText(user['phone'] as String),
+                            buildText(user['website'] as String),
+                            buildText(user['company'] as String),
+                            buildText(user['address'] as String),
+                          ],
+                        ),
+                      ),
+                    ),
                   ),
-                ),
+                ],
               ),
-            )
-          ],
-        ),
-      ),
-    );
+            ),
+          )
+        : const Center(
+            child: CircularProgressIndicator(),
+          );
   }
 }
--- lib/pages/users_page.dart
+++ lib/pages/users_page.dart
@@ -1,9 +1,6 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 
-import '../models/user_model.dart';
+import '../fetch_content.dart';
 import 'user_details_page.dart';
 
 class UsersPage extends StatefulWidget {
@@ -16,22 +13,6 @@ class UsersPage extends StatefulWidget {
 class _UsersPageState extends State<UsersPage> {
   // var to control Loading screen
   var isLoaded = false;
-
-  List<User>? users;
-  // Function to fetch  users from the url
-  Future<List<User>?> fetchUser() async {
-    final response = await http.get(
-      Uri.parse('https://jsonplaceholder.typicode.com/users'),
-    );
-
-    if (response.statusCode == 200) {
-      final parsed = json.decode(response.body).cast<Map<String, dynamic>>();
-
-      return parsed.map<User>((json) => User.fromJson(json)).toList();
-    } else {
-      throw Exception('Failed to load');
-    }
-  }
 
   fetchUsers() async {
     users = await fetchUser();
