9c4529c264fb2a0831b1f68d2cddeccc96c1e49a
Qwerty <thehulk489@gmail.com>
2022-06-12 14:30:13 +0530

    refactored fetching functions into a separate file

---
+++ lib/fetch_content.dart
@@ -0,0 +1,33 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import '../models/user_model.dart';
+import '../models/post_model.dart';
+
+List<User>? users;
+// Function to fetch  users from the url
+Future<List<User>?> fetchUser() async {
+  final response = await http.get(
+    Uri.parse('https://jsonplaceholder.typicode.com/users'),
+  );
+
+  if (response.statusCode == 200) {
+    final parsed = json.decode(response.body).cast<Map<String, dynamic>>();
+
+    return parsed.map<User>((json) => User.fromJson(json)).toList();
+  } else {
+    throw Exception('Failed to load');
+  }
+}
+
+Future<List<Post>?> fetchPostsFromURL() async {
+  final response =
+      await http.get(Uri.parse('http://jsonplaceholder.typicode.com/posts'));
+
+  if (response.statusCode == 200) {
+    final parsed = json.decode(response.body).cast<Map<String, dynamic>>();
+
+    return parsed.map<Post>((json) => Post.fromJson(json)).toList();
+  } else {
+    throw Exception('Failed to load posts');
+  }
+}
