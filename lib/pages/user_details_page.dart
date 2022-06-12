a07d475f851813b334001d6efdc231371a81d24b
Qwerty <thehulk489@gmail.com>
2022-06-12 08:11:04 +0530

    init user_details_page

---
+++ lib/pages/user_details_page.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class DetailsPage extends StatelessWidget {
+  static const routeName = '/user-details';
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
