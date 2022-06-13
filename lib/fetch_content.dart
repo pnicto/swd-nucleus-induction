9fd2f863f3d47c53f10e245281fe3e2dccf613a4
Qwerty <thehulk489@gmail.com>
2022-06-13 08:05:07 +0530

    added fetchComments

--- lib/fetch_content.dart
+++ lib/fetch_content.dart
@@ -3,10 +3,10 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 import '../models/album_model.dart';
+import '../models/comment_model.dart';
 import '../models/post_model.dart';
 import '../models/user_model.dart';
 
-List<User>? users;
 // Function to fetch  users from the url
 Future<List<User>?> fetchUser() async {
   final response = await http.get(
@@ -45,5 +45,18 @@ Future<List<Album>?> fetchAlbumsFromURL() async {
     return parsed.map<Album>((json) => Album.fromJson(json)).toList();
   } else {
     throw Exception('Failed to load albums');
+  }
+}
+
+Future<List<Comment>?> fetchCommentsFromURL() async {
+  final response =
+      await http.get(Uri.parse('http://jsonplaceholder.typicode.com/comments'));
+
+  if (response.statusCode == 200) {
+    final parsed = json.decode(response.body).cast<Map<String, dynamic>>();
+
+    return parsed.map<Comment>((json) => Comment.fromJson(json)).toList();
+  } else {
+    throw Exception('Failed to load comments');
   }
 }
