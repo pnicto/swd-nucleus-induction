fc6f86e5423c11668c24f99f417f64c2c5088929
Qwerty <thehulk489@gmail.com>
2022-07-07 22:34:12 +0530

    Add location and linkedin link button

--- lib/pages/about_me_page.dart
+++ lib/pages/about_me_page.dart
@@ -4,26 +4,19 @@ import 'package:url_launcher/url_launcher.dart';
 import '../widgets/main_drawer.dart';
 
 class AboutMe extends StatelessWidget {
-  AboutMe({Key? key}) : super(key: key);
+  const AboutMe({Key? key}) : super(key: key);
   static const routeName = '/about-me';
-  final Uri _url = Uri.parse('https://github.com/pnicto');
-
-  void _launchUrl() async {
-    if (!await launchUrl(_url)) throw 'Could not launch $_url';
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('About me'),
-        backgroundColor: Colors.black,
       ),
-      backgroundColor: const Color.fromARGB(255, 43, 43, 43),
       drawer: const MainDrawer(),
       body: SingleChildScrollView(
         child: Container(
-          padding: const EdgeInsets.all(5),
+          padding: const EdgeInsets.all(10),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
@@ -31,49 +24,109 @@ class AboutMe extends StatelessWidget {
                 backgroundImage: AssetImage('assets/images/spongebob.webp'),
                 radius: 100,
               ),
-              Container(
-                margin: const EdgeInsets.only(top: 10),
-                child: Card(
-                  elevation: 5,
-                  child: Container(
-                    padding: const EdgeInsets.all(8.0),
-                    child: const Text(
-                      'Hi, I am Dharanikanth studying in BITS Pilani, Hyderabad campus.',
-                      style: TextStyle(
-                        fontSize: 20,
-                      ),
-                    ),
-                  ),
-                ),
+              const SizedBox(
+                height: 10,
               ),
-              const Card(
-                elevation: 5,
-                child: Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Text(
-                    'I don\'t have many hobbies. I usually listen to music.',
+              Card(
+                child: Container(
+                  padding: const EdgeInsets.all(8.0),
+                  child: const Text(
+                    'Hi, I am Dharanikanth studying in BITS Pilani, Hyderabad campus.',
                     style: TextStyle(
                       fontSize: 20,
                     ),
                   ),
                 ),
               ),
-              TextButton(
-                onPressed: _launchUrl,
-                child: Row(
-                  children: const [
-                    Icon(
-                      Icons.link,
+              const SizedBox(
+                height: 10,
+              ),
+              const Card(
+                child: Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text(
+                    'I am from Anantapur.',
+                    style: TextStyle(
+                      fontSize: 20,
                     ),
-                    Padding(
-                      padding: EdgeInsets.all(8.0),
-                      child: Text(
-                        'Github',
-                        textAlign: TextAlign.center,
-                      ),
-                    )
-                  ],
+                  ),
                 ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Card(
+                child: Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text(
+                    'I usually listen to music and play singleplayer games.',
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextButton(
+                    onPressed: () async {
+                      final Uri url = Uri.parse(
+                        'https://github.com/pnicto',
+                      );
+                      if (!await launchUrl(
+                        url,
+                        mode: LaunchMode.externalApplication,
+                      )) {
+                        throw 'Could not launch $url';
+                      }
+                    },
+                    child: Row(
+                      children: const [
+                        Icon(
+                          Icons.link,
+                        ),
+                        Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Text(
+                            'Github',
+                            textAlign: TextAlign.center,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () async {
+                      final Uri url = Uri.parse(
+                        'https://www.linkedin.com/in/dharanikanth-reddy-9386b623b/',
+                      );
+                      if (!await launchUrl(
+                        url,
+                        mode: LaunchMode.externalApplication,
+                      )) {
+                        throw 'Could not launch $url';
+                      }
+                    },
+                    child: Row(
+                      children: const [
+                        Icon(
+                          Icons.link,
+                        ),
+                        Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Text(
+                            'Linkedin',
+                            textAlign: TextAlign.center,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
